ca6acefc54a97e2d86814e4861238cbcf4b45eeb
unknown <dionne.pennycooke@gmail.com>
2023-08-02 14:26:29 +0100

    Media queries added

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export const TodoForm = ({addTodo}) => {
-    const [value, setValue] = useState('');
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
 
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        if (value) {
-          // add todo
-          addTodo(value);
-          // clear form after submission
-          setValue('');
-        }
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value) {
+      addTodo(value);
+      setValue("");
+    }
+  };
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder="Enter today's task here" />
-    <button type="submit" className='todo-btn'>Add Task</button>
-  </form>
-  )
-}
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder="Enter today's task here"
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
